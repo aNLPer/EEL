485607daf056b79528473b7be501992e0e08006f
xuqi <welch_cn@163.com>
2022-08-28 15:49:26 +0800

    train model

--- train/Multi_Task.py
+++ train/Multi_Task.py
@@ -485,7 +485,7 @@ class gru_ljp():
             f.write('valid_f1_records\t' + valid_f1_records + "\n")
             f.write('valid_mr_records\t' + valid_mr_records + "\n")
 
-    def train_wo_charge_aware(self, mode=None):
+    def train_wo_charge_aware(self, mode="charge-aware"):
         """
         :param mode: ["vanilla", "lsscl", "eval_batch"]
         :return:
@@ -581,7 +581,7 @@ class gru_ljp():
             penalty_labels = torch.cat(penalty_labels, dim=0).to(self.device)
             penalty_preds_loss = self.criterion(penalty_preds_outputs, penalty_labels)
 
-            loss = self.ALPHA * (posi_pairs_dist - neg_pairs_dist) + charge_preds_loss + article_preds_loss+ penalty_preds_loss
+            loss = posi_pairs_dist / (posi_pairs_dist+neg_pairs_dist) + charge_preds_loss + article_preds_loss+ penalty_preds_loss
                    # self.LAMDA * penalty_contrains_loss
 
             train_loss += loss.item()
@@ -631,12 +631,11 @@ class gru_ljp():
                         charge_confusMat.updateMat(val_charge_preds.cpu().numpy(), np.array(val_charge_label))
                         article_confusMat.updateMat(val_article_preds.cpu().numpy(), np.array(val_article_label))
                         penalty_confusMat.updateMat(val_penalty_preds.cpu().numpy(), np.array(val_penalty_label))
-
                     val_step += 1
 
                 train_loss_records.append(train_loss / self.EPOCH)
 
-                valid_loss = valid_loss / val_step * self.BATCH_SIZE
+                valid_loss = valid_loss / val_step
                 valid_loss_records.append(valid_loss)
 
                 # acc
@@ -687,16 +686,189 @@ class gru_ljp():
             f.write('valid_f1_records\t' + valid_f1_records + "\n")
             f.write('valid_mr_records\t' + valid_mr_records + "\n")
 
+    def train_wo_lsscl(self, mode="lsscl"):
+        """
+        :param mode: ["vanilla", "lsscl", "eval_batch"]
+        :return:
+        """
+        self.load_model(ablation=mode)
+        # 定义损失函数
+        self.criterion = nn.CrossEntropyLoss()
+
+        # 多分类损失
+        # criterion_mml = nn.MultiMarginLoss()
+
+        # 定义优化器 AdamW由Transfomer提供,目前看来表现很好
+        # optimizer = AdamW(model.parameters(), lr=LR, weight_decay=L2)
+        # optimizer = optim.AdamW(model.parameters(), lr=LR, weight_decay=0.05)
+        optimizer = optim.AdamW([{"params": self.model.em.parameters(), 'lr': 0.00001},
+                                 {"params": self.model.enc.parameters(), 'weight_decay': 0.07},
+                                 {"params": self.model.chargeAwareAtten.parameters(), 'weight_decay': 0.07},
+                                 {'params': self.model.articleAwareAtten.parameters(), 'weight_decay': 0.07},
+                                 {"params": self.model.chargeLinear.parameters()},
+                                 {'params': self.model.chargePreds.parameters()},
+                                 {'params': self.model.articlePreds.parameters()},
+                                 {'params': self.model.penaltyPreds.parameters()}
+                                 ], lr=self.LR, weight_decay=self.L2)
+
+        scheduler = get_cosine_with_hard_restarts_schedule_with_warmup(optimizer,
+                                                                       num_warmup_steps=self.WARMUP_STEP,
+                                                                       num_training_steps=self.STEP,
+                                                                       num_cycles=self.NUM_CYCLES)
+        print("train method start...\n        ")
+
+        train_loss = 0
+        train_loss_records = []
+        valid_loss_records = []
+        valid_acc_records = {"charge": [], "article": [], "penalty": []}
+        valid_mp_records = {"charge": [], "article": [], "penalty": []}
+        valid_f1_records = {"charge": [], "article": [], "penalty": []}
+        valid_mr_records = {"charge": [], "article": [], "penalty": []}
+        for epoch in range(60): # 60个epoch
+            start = time.time()
+            for seqs, accu_labels, article_labels, penalty_labels in data_loader(self.train_seq,
+                                                                               self.train_charge_labels,
+                                                                               self.train_article_labels,
+                                                                               self.train_penalty_labels,
+                                                                               shuffle=True,
+                                                                               batch_size=self.BATCH_SIZE):
+
+                # 设置模型状态
+                self.model.train()
+
+                # 优化参数的梯度置0
+                optimizer.zero_grad()
+
+                seq_lens = []
+                for s in seqs:
+                    seq_lens.append(len(s))
+                for i in range(len(seqs)):
+                    seqs[i] = torch.tensor(seqs[i])
+                padded_input_ids = pad_sequence(seqs, batch_first=True).to(self.device)
+
+                _, charge_preds, article_preds, penalty_preds = self.model(padded_input_ids, seq_lens)
+
+
+                # 指控分类误差
+                charge_preds_loss = self.criterion(charge_preds, torch.tensor(accu_labels).to(self.device))
+
+                # 法律条款预测误差
+                article_preds_loss = self.criterion(article_preds, torch.tensor(article_labels).to(self.device))
+
+                # 刑期预测误差
+                penalty_preds_loss = self.criterion(penalty_preds, torch.tensor(penalty_labels).to(self.device))
+
+                loss = (charge_preds_loss + article_preds_loss+ penalty_preds_loss)/self.BATCH_SIZE
+
+                train_loss += loss.item()
+
+                # 反向传播计算梯度
+                loss.backward()
+
+                # 梯度裁剪防止梯度爆炸
+                nn.utils.clip_grad_norm_(self.model.parameters(), 1.0)
+
+                # 更新梯度
+                optimizer.step()
+
+                # 更新学习率
+                scheduler.step()
+
+            train_loss_records.append(train_loss)
+
+            # 训练完一个EPOCH后评价模型
+            # 初始化混淆矩阵
+            charge_confusMat = ConfusionMatrix(len(self.lang.index2accu))
+            article_confusMat = ConfusionMatrix(len(self.lang.index2art))
+            penalty_confusMat = ConfusionMatrix(self.PENALTY_LABEL_SIZE)
+            # 验证模型在验证集上的表现
+            self.model.eval()
+            valid_loss = 0
+
+            for val_seq, val_charge_label, val_article_label, val_penalty_label in data_loader(self.valid_seq,
+                                                                                               self.valid_charge_labels,
+                                                                                               self.valid_article_labels,
+                                                                                               self.valid_penalty_labels,
+                                                                                               shuffle=False,
+                                                                                               batch_size=10 * self.BATCH_SIZE):
+                val_seq_lens = [len(s) for s in val_seq]
+                val_input_ids = [torch.tensor(s) for s in val_seq]
+                val_input_ids = pad_sequence(val_input_ids, batch_first=True).to(self.device)
+                with torch.no_grad():
+                    _, val_charge_preds, val_article_preds, val_penalty_preds = self.model(val_input_ids,
+                                                                                                    val_seq_lens)
+                    val_charge_preds_loss = self.criterion(val_charge_preds, torch.tensor(val_charge_label).to(self.device))
+                    val_article_preds_loss = self.criterion(val_article_preds,
+                                                       torch.tensor(val_article_label).to(self.device))
+                    val_penalty_preds_loss = self.criterion(val_penalty_preds,
+                                                       torch.tensor(val_penalty_label).to(self.device))
+
+                    valid_loss += (val_charge_preds_loss.item()+val_article_preds_loss.item()+val_penalty_preds_loss.item())/10*self.BATCH_SIZE
+
+                    charge_confusMat.updateMat(val_charge_preds.cpu().numpy(), np.array(val_charge_label))
+                    article_confusMat.updateMat(val_article_preds.cpu().numpy(), np.array(val_article_label))
+                    penalty_confusMat.updateMat(val_penalty_preds.cpu().numpy(), np.array(val_penalty_label))
+
+            valid_loss_records.append(valid_loss)
+
+            # acc
+            valid_acc_records['charge'].append(charge_confusMat.get_acc())
+            valid_acc_records['article'].append(article_confusMat.get_acc())
+            valid_acc_records['penalty'].append(penalty_confusMat.get_acc())
+
+            # F1
+            valid_f1_records['charge'].append(charge_confusMat.getMaF())
+            valid_f1_records['article'].append(article_confusMat.getMaF())
+            valid_f1_records['penalty'].append(penalty_confusMat.getMaF())
+
+            # MR
+            valid_mr_records['charge'].append(charge_confusMat.getMaR())
+            valid_mr_records['article'].append(article_confusMat.getMaR())
+            valid_mr_records['penalty'].append(penalty_confusMat.getMaR())
+
+            # MP
+            valid_mp_records['charge'].append(charge_confusMat.getMaP())
+            valid_mp_records['article'].append(article_confusMat.getMaP())
+            valid_mp_records['penalty'].append(penalty_confusMat.getMaP())
+
+            end = time.time()
+            print(
+                f"Epoch: {epoch}  Train_loss: {round(train_loss / self.EPOCH, 6)}  Valid_loss: {round(valid_loss, 6)} \n"
+                f"Charge_Acc: {round(charge_confusMat.get_acc(), 6)}  Charge_F1: {round(charge_confusMat.getMaF(), 6)}  Charge_MR: {round(charge_confusMat.getMaR(), 6)}  Charge_MP: {round(charge_confusMat.getMaP(), 6)}\n"
+                f"Article_Acc: {round(article_confusMat.get_acc(), 6)}  Article_F1: {round(article_confusMat.getMaF(), 6)}  Article_MR: {round(article_confusMat.getMaR(), 6)}  Article_MP: {round(article_confusMat.getMaP(), 6)}\n"
+                f"Penalty_Acc: {round(penalty_confusMat.get_acc(), 6)}  Penalty_F1: {round(penalty_confusMat.getMaF(), 6)}  Penalty_MR: {round(penalty_confusMat.getMaR(), 6)}  Penalty_MP: {round(penalty_confusMat.getMaP(), 6)}\n"
+                f"Time: {round((end - start) / 60, 2)}min ")
+
+            # 保存模型
+            save_path = f"../dataset/checkpoints/model-at-epoch-{mode}-{self.BATCH_SIZE}-{self.SIM_ACCU_NUM}.pt"
+            torch.save(self.model, save_path)
+
+            train_loss = 0
+
+        train_loss_records = json.dumps(train_loss_records, ensure_ascii=False)
+        valid_loss_records = json.dumps(valid_loss_records, ensure_ascii=False)
+        valid_acc_records = json.dumps(valid_acc_records, ensure_ascii=False)
+        valid_mp_records = json.dumps(valid_mp_records, ensure_ascii=False)
+        valid_f1_records = json.dumps(valid_f1_records, ensure_ascii=False)
+        valid_mr_records = json.dumps(valid_mr_records, ensure_ascii=False)
+        with open(f"../training_records_{mode}_{self.BATCH_SIZE}_{self.SIM_ACCU_NUM}.txt", "w", encoding="utf-8") as f:
+            f.write('train_loss_records\t' + train_loss_records + "\n")
+            f.write('valid_loss_records\t' + valid_loss_records + "\n")
+            f.write('valid_acc_records\t' + valid_acc_records + "\n")
+            f.write('valid_mp_records\t' + valid_mp_records + "\n")
+            f.write('valid_f1_records\t' + valid_f1_records + "\n")
+            f.write('valid_mr_records\t' + valid_mr_records + "\n")
+
 
 def verify_sim_accu():
     "验证SIM_ACCU_NUM对模型的影响"
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    SIM_ACCU_NUM = [2, 4, 8, 16, 32]
+    SIM_ACCU_NUM = [1]
     for i in range(len(SIM_ACCU_NUM)):
         print(f"\n-----------------------SIM_ACCU_NUM={SIM_ACCU_NUM[i]}------------------------\n")
         ljp = gru_ljp(device=device, section="multi-task")
         ljp.SIM_ACCU_NUM = SIM_ACCU_NUM[i]
-        ljp.train(mode=None)
+        ljp.train()
         del ljp
         torch.cuda.empty_cache()
 
@@ -720,6 +892,14 @@ def verify_LSSCL():
     del ljp
     torch.cuda.empty_cache()
 
+def verify_chargeAware():
+    "验证LSSCL对模型的影响"
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    ljp = gru_ljp(device=device, section="multi-task")
+    ljp.train_wo_charge_aware()
+    del ljp
+    torch.cuda.empty_cache()
+
 def veryfy_LSSCL_BERT():
     "验证LSSCL方法微调BERT的效果"
     pass
@@ -732,13 +912,16 @@ if __name__=="__main__":
 
 
     # "训练集衰减对模型影响"
-    verify_trainset_decay()
+    # verify_trainset_decay()
 
     # 验证SIM_ACCU_NUM对模型的影响
     # verify_sim_accu()
 
     # "验证LSSCL对模型的影响"
     # verify_LSSCL()
+
+    # "验证charge_aware对模型的影响"
+    verify_chargeAware()
 
     # 验证charge-aware-attention对模型的影响
     # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
